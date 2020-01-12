ee06839b429b962d61060ce1d6e2f1e197e0a184
Mohit <mohit.gupta7860@gmail.com>
2020-01-11 23:21:31 -0500

    Initial Commit

---
+++ GitCommitApiSample/GitCommitApiSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitCommitApiSample
+//
+//  Created by Mohit on 11/01/20.
+//  Copyright © 2020 Mohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
---
+++ GitCommitApiSample/GitCommitApiSampleTests/GitCommitApiSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitCommitApiSampleTests.swift
+//  GitCommitApiSampleTests
+//
+//  Created by Mohit on 11/01/20.
+//  Copyright © 2020 Mohit. All rights reserved.
+//
+
+import XCTest
+@testable import GitCommitApiSample
+
+class GitCommitApiSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
